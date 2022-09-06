36b31dc150fcf232a7888e6b491fd25a596a5c57
treetips <treetips555@gmail.com>
2022-09-06 22:17:58 +0900

    fix: fix eslint path error

--- vite.config.ts
+++ vite.config.ts
@@ -5,30 +5,38 @@ import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  root: "./src",
-  server: {
-    host: "0.0.0.0",
-    open: true,
-  },
-  build: {
-    outDir: "../build",
-    emptyOutDir: true,
-  },
-  plugins: [
-    react(),
-    checker({
-      typescript: true,
-      vueTsc: false,
-      eslint: {
-        lintCommand: 'eslint "../src/**/*.{ts,tsx}"',
+export default defineConfig(
+  (
+    // https://ja.vitejs.dev/config/#%E6%9D%A1%E4%BB%B6%E4%BB%98%E3%81%8D%E8%A8%AD%E5%AE%9A
+    { command }
+  ) => {
+    const src = command === "serve" ? "../src" : "./src";
+    return {
+      base: "./",
+      root: "./src",
+      server: {
+        host: "0.0.0.0",
+        open: true,
       },
-    }),
-  ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./vitest.setup.ts",
-  },
-});
+      build: {
+        outDir: "../build",
+        emptyOutDir: true,
+      },
+      plugins: [
+        react(),
+        checker({
+          typescript: true,
+          vueTsc: false,
+          eslint: {
+            lintCommand: `eslint "${src}/**/*.{ts,tsx}"`,
+          },
+        }),
+      ],
+      test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./vitest.setup.ts",
+      },
+    };
+  }
+);

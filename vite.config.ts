5984f8dfa1fac43e4ac65c83ad411eca385d1753
treetips <treetips555@gmail.com>
2025-06-27 02:17:22 +0900

    refactor: response to biome warning

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(
   (
     // https://ja.vitejs.dev/config/#%E6%9D%A1%E4%BB%B6%E4%BB%98%E3%81%8D%E8%A8%AD%E5%AE%9A
-    { command, mode }
+    { mode }
   ) => {
-    const src = command === "serve" ? "../src" : "./src";
     return {
       base: "./",
       root: "./src",
